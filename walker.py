da65ee70ca6370d48a2d0a88cdd1a59be288881a
fodrh <fodrh1201@gmail.com>
2015-08-16 12:28:14 +0900

    walker recursive version but fail

--- walker.py
+++ walker.py
@@ -1,5 +1,42 @@
 
 class Walker():
 
-    def __init__(self, corners):
-        self.corners = corners
+    def __init__(self, root, direction, graph):
+        self.root = root
+        self.direction = direction
+        self.current_corner = root
+        self.quad_list = [root]
+        self.graph = graph
+
+        if root is None:
+            return
+        self.start()
+
+    def start(self):
+        def when_up():
+            self.current_corner = self.root.up
+            self.turn_left()
+            self.turn_right()
+            self.graph.start_walker(Walker(self.root.up, 'up', self.graph))
+
+        def when_down():
+            self.current_corner = self.current_corner.down
+
+        def when_left():
+            self.current_corner = self.current_corner.left
+
+        def when_right():
+            self.current_corner = self.current_corner.right
+
+        return {
+            'up': when_up,
+            'down': when_down,
+            'right': when_right,
+            'left': when_left
+        }[self.direction]()
+
+    def turn_left(self):
+        return
+
+    def turn_right(self):
+        return
